b0a20f92bdee24c083a4878ca232d7e637836b76
AlanTuring06 <69383846+AlanTuring06@users.noreply.github.com>
2021-06-17 15:01:43 +0700

    Create main.py

---
+++ Easy/Tic Tac Toe/Stage 2/main.py
@@ -0,0 +1,8 @@
+# write your code here
+only = ['X', 'O', '_']
+input = str(input("Enter cells:"))
+print("---------")
+print("| " + input[0] + " " + input[1] + " " + input[2] + " |") 
+print("| " + input[3] + " " + input[4] + " " + input[5] + " |") 
+print("| " + input[6] + " " + input[7] + " " + input[8] + " |") 
+print("---------")
